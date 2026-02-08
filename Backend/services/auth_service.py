c7c674c6366d82032b1312ec72a6e5f1ed522104
Nemofiii <namrahjuweriasayed@gmail.com>
2026-02-09 00:02:34 +0530

    Updated

--- Backend/services/auth_service.py
+++ Backend/services/auth_service.py
@@ -2,11 +2,20 @@ from sqlalchemy.orm import Session
 from passlib.hash import bcrypt
 from Backend.models.user import User
 from Backend.core.security import create_token, decode_token
+from fastapi import HTTPException, status
 
 def register(db: Session, email: str, password: str):
+    existing_user = db.query(User).filter(User.email == email).first()
+
+    if existing_user:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="Email already registered"
+        )
+    
     hashed_password = bcrypt.hash(password)
     user = User(email=email, password_hash=hashed_password)
-
+    
     db.add(user)
     db.commit()
     db.refresh(user)
